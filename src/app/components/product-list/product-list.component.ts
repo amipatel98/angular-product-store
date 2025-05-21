d26bf64bf9980b911026465f12c7dc56e09e2317
Ami Patel <patelami719@gmail.com>
2025-05-21 18:45:50 +0530

    display product store

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { TableModule } from 'primeng/table';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
+import { StyleClassModule } from 'primeng/styleclass';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [
+    TableModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
+    PaginatorModule,
+    StyleClassModule,
+  ],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.scss',
+})
+export class ProductListComponent {
+  productList: any[] = [];
+  searchProduct: string = '';
+  filteredProductList: any[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.productList = data;
+      this.filteredProductList = data;
+      // console.log('productList', this.productList);
+    });
+  }
+
+  filterProductList() {
+    const term = this.searchProduct.toLowerCase();
+    this.filteredProductList = this.productList.filter(
+      (product) =>
+        product.title.toLowerCase().includes(term) ||
+        product.category.toLowerCase().includes(term)
+    );
+  }
+}
