bf539bff1125bb07b7151a0f8f72fa4981dffe72
Ami Patel <patelami719@gmail.com>
2025-05-23 13:37:22 +0530

    changes for table layout

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { PaginatorModule } from 'primeng/paginator';
 import { StyleClassModule } from 'primeng/styleclass';
+import { RatingModule } from 'primeng/rating';
 
 @Component({
   selector: 'app-product-list',
@@ -17,6 +18,7 @@ import { StyleClassModule } from 'primeng/styleclass';
     CommonModule,
     PaginatorModule,
     StyleClassModule,
+    RatingModule,
   ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
@@ -25,13 +27,14 @@ export class ProductListComponent {
   productList: any[] = [];
   searchProduct: string = '';
   filteredProductList: any[] = [];
-
+  loading: boolean = true;
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
       this.productList = data;
       this.filteredProductList = data;
+      this.loading = false;
       // console.log('productList', this.productList);
     });
   }
